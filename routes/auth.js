182abcd81dd1070955c16a3a8dcb441fc2befc4d
Minte123 <netacademy1986minte@yahoo.com>
2022-12-01 12:46:33 +0300

    blog api

---
+++ routes/auth.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPass,
+    });
+
+    const user = await newUser.save();
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if(!user){
+      return res.status(400).json("Wrong credentials!");
+    }
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    if(!validated){ 
+      return res.status(400).json("Wrong credentials!");
+    }
+
+    const { password, ...others } = user._doc;
+    return res.status(200).json(others);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ routes/posts.js
@@ -0,0 +1,93 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const Post = require("../models/Post");
+
+//CREATE POST
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    return res.status(200).json(savedPost);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//UPDATE POST
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        return res.status(200).json(updatedPost);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    } else {
+      return res.status(401).json("You can update only your post!");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//DELETE POST
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        return res.status(200).json("Post has been deleted...");
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    } else {
+      return res.status(401).json("You can delete only your post!");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//GET POST
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    return res.status(200).json(post);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//GET ALL POSTS
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find();
+    }
+    return res.status(200).json(posts);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+module.exports = router;
